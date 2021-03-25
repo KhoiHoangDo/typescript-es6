0bddaac07e7e53da8c25408667d529aba85e568e
khoidh <dohoangkhoi19992812@gmail.com>
2021-03-25 16:41:09 +0700

    lesson 5 Ke Thua class trong ES6

---
+++ lesson5.js
@@ -0,0 +1,22 @@
+class Person{
+    constructor(name){
+        console.log(name + ' Person contructor');
+    }
+
+    getID(){
+        return 10;
+    }
+}
+
+class Employee extends Person{
+    constructor(name){
+        super(name);
+        console.log(name + ' Employee Contructor')
+    }
+    getID(){
+        return super.getID();
+    }
+}
+
+let emp = new Employee("Hoang Khoi");
+console.log(emp.getID());
